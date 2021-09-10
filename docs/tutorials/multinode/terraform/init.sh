ec63151922ce7c7c1ee026ca5b442ff7e6eb2df2
Burt Holzman <burt@fnal.gov>
2021-09-10 11:02:50 -0500

    Add support for regional instance groups
    To enable regional (rather than zonal) instance groups, set
    TF_VAR_multizone to True and TF_VAR_numzones to the number
    of zones in the region.

--- docs/tutorials/multinode/terraform/init.sh
+++ docs/tutorials/multinode/terraform/init.sh
@@ -4,7 +4,8 @@ export TF_VAR_project=peters-4
 export TF_VAR_project_id=peters-4
 export TF_VAR_zone=us-central1-b
 export TF_VAR_region=us-central1
-
+export TF_VAR_multizone=true
+export TF_VAR_numzones=4  # for regional/multizone, set to the number of regions in the zone
 # ---- Do not edit below -----#
 
 export TF_VAR_project_id=${TF_VAR_project}
