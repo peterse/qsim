34ba51ec6eb6d663c6e5dd04589c77546a77ce82
evan <peterse583@gmail.com>
2021-09-15 22:53:19 -0700

    gpu.sh

--- docs/tutorials/multinode/gpu.sh
+++ docs/tutorials/multinode/gpu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# You must source the installed drivers and libraries as they appear in the
+# terraform startup script.
 export PATH="/usr/local/cuda-11.4/bin${PATH:+:${PATH}}"
 export PYTHONPATH=$PYTHONPATH:"/home/peterse583/qsim"
 # export PATH="/usr/local/cuda-11.4/bin${PATH:+:${PATH}}"
