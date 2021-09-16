5be89375a70466f78cd2928cdc57557b2d1e5cd5
evan <peterse583@gmail.com>
2021-09-15 20:49:52 -0700

    env from sub file

--- docs/tutorials/multinode/gpu.sh
+++ docs/tutorials/multinode/gpu.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+export PATH="/usr/local/cuda-11.4/bin${PATH:+:${PATH}}"
+export PYTHONPATH=$PYTHONPATH:"/home/peterse583/qsim"
+# export PATH="/usr/local/cuda-11.4/bin${PATH:+:${PATH}}"
+# export PYTHONPATH=$PYTHONPATH:"$PWD"
 python3 noise4.py
